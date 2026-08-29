d29fd9f770faaaffc2582bd7c7a4d2926bdc1b37
prachotx <prachot098@gmail.com>
2026-08-29 15:36:57 +0700

    Add detailed School serializers and update SchoolViewSet for classroom, student, and teacher counts

--- apis/serializers.py
+++ apis/serializers.py
@@ -4,10 +4,36 @@ from .models import ClassRoom
 from .models import Student
 from .models import Teacher
 
-class SchoolSerializer(serializers.ModelSerializer):
+class SchoolListSerializer(serializers.ModelSerializer):
     class Meta:
         model = School
         fields = "__all__"
+
+class SchoolDetailSerializer(serializers.ModelSerializer):
+
+    classroom_count = serializers.IntegerField(
+        read_only=True
+    )
+
+    student_count = serializers.IntegerField(
+        read_only=True
+    )
+
+    teacher_count = serializers.IntegerField(
+        read_only=True
+    )
+
+    class Meta:
+        model = School
+        fields = [
+            "id",
+            "name",
+            "short_name",
+            "address",
+            "classroom_count",
+            "student_count",
+            "teacher_count",
+        ]
         
 class ClassRoomSerializer(serializers.ModelSerializer):
     class Meta:
--- apis/views/v1/school.py
+++ apis/views/v1/school.py
@@ -1,11 +1,12 @@
 from apis.models import School
-from apis.serializers import SchoolSerializer
+from apis.serializers import SchoolListSerializer
+from apis.serializers import SchoolDetailSerializer
 from rest_framework.filters import SearchFilter
 from rest_framework import viewsets
+from django.db.models import Count
 
 class SchoolViewSet(viewsets.ModelViewSet):
-    queryset = School.objects.all()
-    serializer_class = SchoolSerializer
+    serializer_class = SchoolListSerializer
 
     filter_backends = [
         SearchFilter
@@ -14,3 +15,30 @@ class SchoolViewSet(viewsets.ModelViewSet):
     search_fields = [
         "name"
     ]
+
+    def get_queryset(self):
+        queryset = School.objects.all()
+
+        if self.action == "retrieve":
+            queryset = queryset.annotate(
+                classroom_count=Count(
+                    "class_rooms",
+                    distinct=True
+                ),
+                student_count=Count(
+                    "class_rooms__students",
+                    distinct=True
+                ),
+                teacher_count=Count(
+                    "class_rooms__teachers",
+                    distinct=True
+                ),
+            )
+
+        return queryset
+
+    def get_serializer_class(self):
+        if self.action == "retrieve":
+            return SchoolDetailSerializer
+
+        return SchoolListSerializer
